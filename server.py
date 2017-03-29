29c92d07229c79864e1bf11f5a545d8e1145d12b
Ephraim D. Rosenfeld <edr292@nyu.edu>
2017-03-29 00:39:26 -0400

    initial update of test cases to meet coverage requirements

--- server.py
+++ server.py
@@ -42,6 +42,7 @@ def list_products():
     max_price       = request.args.get('max-price')
     price           = request.args.get('price')
     limit           = request.args.get('limit')
+    search_term     = request.args.get('search')
     results         = []
     count           = 0
     cutoff          = 10
@@ -54,10 +55,11 @@ def list_products():
     for key, value in products.iteritems():
         if (count == cutoff):
             break
-        if matches_clause(category,      value['category']) and \
-           matches_clause(discontinued,  value['discontinued']) and \
-           matches_price(min_price, max_price, price, value['price']):
-                results.append(products[key])
+        if matches_clause(  category,       value['category']) and \
+           matches_clause(  discontinued,   value['discontinued']) and \
+           matches_price(   min_price,      max_price, price, value['price']) and\
+           matches_term(    search_term,    value):
+            results.append(products[key])
         count += 1
 
     return reply(results, HTTP_200_OK)
@@ -69,6 +71,12 @@ def matches_price(min_price, max_price, price, value):
 
 def matches_clause(clause_value, item_value):
     return clause_value is None or clause_value == item_value 
+
+def matches_term(term, product):
+    return  term is None or \
+            term.lower() in product['category']     .lower().split() or \
+            term.lower() in product['name']         .lower().split() or \
+            term.lower() in product['description']  .lower().split()
 
 ######################################################################
 # RETRIEVE A product
@@ -174,15 +182,16 @@ def get_product_data():
     with open(PRODUCTS_DATA_SOURCE_FILE) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            prod_data[int(row['id'])] = {   'id':int(row['id']), 'name':row['name'], 'category':row['category'], \
-                                            'discontinued': row.get('discontinued', False), \
-                                            'price': int(row['price'])   }
+            prod_data[int(row['id'])] = {   'id':           int(row['id']),     'name':         row['name'], \
+                                            'category':     row['category'],    'discontinued': row.get('discontinued', False), \
+                                            'price':        int(row['price']),  'description':  row.get('description', '')   }
     return prod_data
 
 def insertUpdateProdEntry(product_id, products, json_payload):
-    products[product_id] = {'id': product_id, 'name': json_payload['name'], 'category': json_payload['category']}
+    products[product_id] = {'id':       product_id,                 'name':  json_payload['name'], 
+                            'category': json_payload['category'],   'price': json_payload['price'] }
     products[product_id]['discontinued'] = json_payload.get('discontinued', False)
-    products[product_id]['price'] = json_payload.get('price')
+    products[product_id]['description']  = json_payload.get('description',  '')
     
 def str2bool(value):
   return None if value is None else value.lower() in ("yes", "true", "t", "1")
@@ -191,8 +200,8 @@ def str2bool(value):
 ######################################################################
 products = {}
 if __name__ == "__main__":
-    products = get_product_data();
+    products    = get_product_data();
     # Pull options from environment
-    debug = (os.getenv('DEBUG', 'False') == 'True')
-    port = os.getenv('PORT', '5000')
+    debug       = (os.getenv('DEBUG', 'False') == 'True')
+    port        = os.getenv('PORT', '5000')
     app.run(host='0.0.0.0', port=int(port), debug=debug)
--- test_server.py
+++ test_server.py
@@ -1,5 +1,6 @@
 # run with:
 # python -m unittest discover
+# nosetests --exe -v --rednose --nologcapture
 
 import unittest
 import json
@@ -21,118 +22,157 @@ class TestProductServer(unittest.TestCase):
     def setUp(self):
         server.app.debug = True
         self.app = server.app.test_client()
-        server.products = { 1: {'id': 1, 'name': 'TV', 'category': 'entertainment', 'discontinued': True, 'price': 999}, 2: {'id': 2, 'name': 'Blender', 'category': 'appliances', 'discontinued': False, 'price': 99} }   
+        server.products = { 1: {'id':           1,      'name':     'TV',               'category':     'entertainment', 
+                                'discontinued': True,   'price':    999,                'description':  'Flat Screen LCD'}, 
+                           2: {'id':            2,      'name':     'Electric Blender', 'category':     'kitchen appliances', 
+                               'discontinued': False,   'price':    99,                 'description' : 'European-made immersion blender'} 
+                           }   
 
     def test_index(self):
         resp = self.app.get('/')
-        self.assertTrue ('product Demo REST API Service' in resp.data)
-        self.assertTrue( resp.status_code == HTTP_200_OK )
+        self.assertIn ('product Demo REST API Service', resp.data)
+        self.assertEqual(resp.status_code, HTTP_200_OK, 'Status should be HTTP_200_OK')
 
     def test_get_product_list(self):
         resp = self.app.get('/products')
-        self.assertTrue( resp.status_code == HTTP_200_OK )
+        self.assertEqual(resp.status_code, HTTP_200_OK, 'Status should be HTTP_200_OK' )
 
         data = json.loads(resp.data)
         self.assertTrue( len(resp.data) > 0 ) 
-        self.assertTrue(data[0]['name'] == 'TV')
-        self.assertTrue(data[1]['name'] == 'Blender')
+        self.assertEqual(data[0]['id'], 1)
+        self.assertEqual(data[1]['id'], 2)
 
     def test_get_product_price(self):
         resp_min_price = self.app.get('/products?min-price=100')
-        self.assertTrue( resp_min_price.status_code == HTTP_200_OK )
+        self.assertEqual(resp_min_price.status_code, HTTP_200_OK)
         data_min_price = json.loads(resp_min_price.data)
-        self.assertTrue(data_min_price[0]['name'] == 'TV')
+        self.assert_single_value_by_id(data_min_price, 1)
         
         resp_max_price = self.app.get('/products?max-price=100')
         self.assertTrue( resp_max_price.status_code == HTTP_200_OK )
         data_max_price = json.loads(resp_max_price.data)
-        self.assertTrue(data_max_price[0]['name'] == 'Blender')
+        self.assert_single_value_by_id(data_max_price, 2)
 
     def test_get_product_limit(self):
-        resp = self.app.get('/products?limit=1')
-        self.assertTrue( resp.status_code == HTTP_200_OK )
-        data = json.loads(resp.data)
-        self.assertTrue( len(data) == 1)
+        valid_limit_resp = self.app.get('/products?limit=1')
+        self.assertEqual(valid_limit_resp.status_code, HTTP_200_OK)
+        data = json.loads(valid_limit_resp.data)
+        self.assertEqual(len(data), 1)
+        
+        invalid_limit_resp = self.app.get('/products?limit=-1')
+        self.assertEqual(invalid_limit_resp.status_code, HTTP_400_BAD_REQUEST)
 
     def test_get_product_category(self):
         resp = self.app.get('/products?category=entertainment')
-        self.assertTrue( resp.status_code == HTTP_200_OK )
+        self.assertEqual(resp.status_code, HTTP_200_OK)
         data = json.loads(resp.data)
-        self.assertTrue(data[0]['name'] == 'TV')
+        self.assert_single_value_by_id(data, 1)
+        
+    def test_get_product_search(self):
+        search_by_name_resp = self.app.get('/products?search=blender')
+        self.assertEqual(search_by_name_resp.status_code, HTTP_200_OK)
+        data                = json.loads(search_by_name_resp.data)
+        self.assert_single_value_by_id(data, 2)
+        
+        search_by_category_resp = self.app.get('/products?search=kitchen')
+        self.assertTrue( search_by_category_resp.status_code == HTTP_200_OK )
+        data                    = json.loads(search_by_category_resp.data)
+        self.assert_single_value_by_id(data, 2)
+        
+        search_by_descrip_resp  = self.app.get('/products?search=Immersion')
+        self.assertTrue( search_by_descrip_resp.status_code == HTTP_200_OK )
+        data                    = json.loads(search_by_descrip_resp.data)
+        self.assert_single_value_by_id(data, 2)
+    
 
     def test_get_product(self):
         resp = self.app.get('/products/2')
         self.assertTrue( resp.status_code == HTTP_200_OK )
         data = json.loads(resp.data)
-        self.assertTrue (data['name'] == 'Blender')
+        self.assertEqual(data['id'],  2)
 
     def test_query_product_list(self):
-        self.setup_test_by_attribute('category', 'entertainment')
-        self.setup_test_by_attribute('discontinued', True)  
-        self.setup_test_by_attribute('price', 999)  
+        self.setup_test_by_attribute('category',        'entertainment')
+        self.setup_test_by_attribute('discontinued',    True)  
+        self.setup_test_by_attribute('price',           999)  
 
     def test_create_product(self):
         # save the current number of products for later comparison
-        product_count = self.get_product_count()
+        product_count   = self.get_product_count()
         # add a new product
-        new_product = {'name': 'toaster', 'category': 'kitchen appliances', 'price': 99}
-        data = json.dumps(new_product)
-        resp = self.app.post('/products', data=data, content_type='application/json')
-        self.assertTrue( resp.status_code == HTTP_201_CREATED )
-        new_json = json.loads(resp.data)
-        self.assertTrue (new_json['name'] == 'toaster')
+        new_product     = {'name': 'toaster', 'category': 'kitchen appliances', 'price': 99}
+        data            = json.dumps(new_product)
+        resp            = self.app.post('/products', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, HTTP_201_CREATED)
+        new_json        = json.loads(resp.data)
+        self.assertEqual(new_json['name'], 'toaster')
         # check default value of 'discontinued' is 'False'
-        self.assertTrue (new_json['discontinued'] == False)
+        self.assertEqual(new_json['discontinued'], False)
         # check the price of the product
-        self.assertTrue (new_json['price'] == 99)
-        # check that count has gone up and includes sammy
+        self.assertEqual(new_json['price'], 99)
+        # check that count has gone up
         resp = self.app.get('/products?limit=' + str(product_count + 1))
         data = json.loads(resp.data)
-        self.assertTrue( resp.status_code == HTTP_200_OK )
-        self.assertTrue( len(data) == product_count + 1 )
-        self.assertTrue( new_json in data )
+        self.assertEqual(resp.status_code,  HTTP_200_OK)
+        self.assertEqual(len(data),         product_count + 1 )
+        self.assertIn( new_json, data )
 
     def test_update_product(self):
-        new_Blender = {'name': 'Blender', 'category': 'home appliances', 'discontinued': True, 'price': 99}
-        data = json.dumps(new_Blender)
+        test_product_id             = 2
+        test_name                   = 'Immersion Blender'
+        test_category               = 'home appliances'
+        test_discontinued_status    = True
+        test_price                  = 50
+        test_description            = 'This is a test description'
+        update_prod = { 'id':           test_product_id,            'name':     test_name,  'category':     test_category, 
+                        'discontinued': test_discontinued_status,   'price':    test_price, 'description':  test_description }
+        data = json.dumps(update_prod)
         resp = self.app.put('/products/2', data=data, content_type='application/json')
-        self.assertTrue( resp.status_code == HTTP_200_OK )
+        self.assertEqual(resp.status_code, HTTP_200_OK )
         new_json = json.loads(resp.data)
-        self.assertTrue (new_json['category']       == 'home appliances')
-        self.assertTrue (new_json['discontinued']   == True)
-        self.assertTrue (new_json['price']          == 99)
+        self.assertEqual(sorted(new_json.items()), sorted(update_prod.items()))
+        #Attempt to update a record whose ID is not found
+        unfound_product_id  = 999
+        update_prod['id']     = unfound_product_id
+        data = json.dumps(update_prod)
+        resp = self.app.put('/products/'+str(unfound_product_id), data=data, content_type='application/json')
+        self.assertEqual(resp.status_code,  HTTP_404_NOT_FOUND)
     
     def test_discontinue_produce(self):
         resp = self.app.put('/products/2/discontinue')
-        self.assertTrue( resp.status_code == HTTP_200_OK )
+        self.assertEqual(resp.status_code,          HTTP_200_OK )
         new_json = json.loads(resp.data)
-        self.assertTrue (new_json['discontinued']   == True)
+        self.assertEqual(new_json['discontinued'],  True)#fixme - Reset this
+        
+        unfound_product_id  = 999
+        resp = self.app.put('/products/%d/discontinue' % (unfound_product_id))
+        self.assertEqual(resp.status_code,  HTTP_404_NOT_FOUND)
 
     def test_update_product_with_no_name(self):
         new_product = {'category': 'entertainment'}
         data = json.dumps(new_product)
         resp = self.app.put('/products/2', data=data, content_type='application/json')
-        self.assertTrue( resp.status_code == HTTP_400_BAD_REQUEST )
+        self.assertEqual(resp.status_code, HTTP_400_BAD_REQUEST )
 
     def test_delete_product(self):
         # save the current number of products for later comparrison
         product_count = self.get_product_count()
         # delete a product
-        resp = self.app.delete('/products/2', content_type='application/json')
-        self.assertTrue( resp.status_code == HTTP_204_NO_CONTENT )
-        self.assertTrue( len(resp.data) == 0 )
-        new_count = self.get_product_count()
-        self.assertTrue ( new_count == product_count - 1)
+        resp        = self.app.delete('/products/2', content_type='application/json')
+        self.assertEqual(resp.status_code,  HTTP_204_NO_CONTENT)
+        self.assertEqual(len(resp.data),    0 )
+        new_count   = self.get_product_count()
+        self.assertEqual(new_count, product_count - 1)
 
     def test_create_product_with_no_name(self):
         new_product = {'category': 'entertainment'}
         data = json.dumps(new_product)
         resp = self.app.post('/products', data=data, content_type='application/json')
-        self.assertTrue( resp.status_code == HTTP_400_BAD_REQUEST )
+        self.assertEqual(resp.status_code, HTTP_400_BAD_REQUEST)
 
     def test_get_nonexisting_product(self):
         resp = self.app.get('/products/5')
-        self.assertTrue( resp.status_code == HTTP_404_NOT_FOUND )
+        self.assertEqual(resp.status_code, HTTP_404_NOT_FOUND)
 
 ######################################################################
 # Utility functions
@@ -141,19 +181,23 @@ class TestProductServer(unittest.TestCase):
     def get_product_count(self):
         # save the current number of products
         resp = self.app.get('/products')
-        self.assertTrue( resp.status_code == HTTP_200_OK )
+        self.assertEqual(resp.status_code, HTTP_200_OK)
         # print 'resp_data: ' + resp.data
         data = json.loads(resp.data)
         return len(data)
 
     def setup_test_by_attribute(self, attr_name, attr_value):
-        resp = self.app.get('/products', \
+        resp        = self.app.get('/products', \
                             query_string='attr_name=attr_value'.format(attr_name=attr_name, attr_value=attr_value))
-        self.assertTrue( resp.status_code == HTTP_200_OK )
+        self.assertEqual(resp.status_code, HTTP_200_OK)
         self.assertTrue( len(resp.data) > 0 )
-        data = json.loads(resp.data)
-        query_item = data[0]
-        self.assertTrue(query_item[attr_name] == attr_value) 
+        data        = json.loads(resp.data)
+        query_item  = data[0]
+        self.assertEqual(query_item[attr_name], attr_value) 
+        
+    def assert_single_value_by_id(self, prod_test_data, prod_id):
+        self.assertEqual(len(prod_test_data),       1, 'data must be only single value')
+        self.assertEqual(prod_test_data[0]['id'],   prod_id)    
 ######################################################################
 #   M A I N
 ######################################################################
